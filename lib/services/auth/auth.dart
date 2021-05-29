5a156ab50896e6ed551a178b02a44e09d312ba95
alinaleehx <alinaleehx@gmail.com>
2021-05-29 11:38:41 +0800

    Add authentication service using firebase_auth.

---
+++ lib/services/auth/auth.dart
@@ -0,0 +1,15 @@
+import 'package:stockup/models/appUser.dart';
+
+abstract class AuthService {
+  // auth change AppUser stream
+  Stream<AppUser> get user;
+
+  // sign in using email and password
+  Future signInWithEmailPassword(String email, String password);
+
+  // register with email and password
+  Future registerWithEmailPassword(String email, String password);
+
+  // sign out
+  Future signOut();
+}
---
+++ lib/services/auth/auth_impl.dart
@@ -0,0 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:stockup/services/auth/auth.dart';
+import 'package:stockup/models/appUser.dart';
+
+class AuthImplementation extends AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create User object based on firebase
+  AppUser _appUser(User user) {
+    return user != null ? AppUser(uid: user.uid) : null;
+  }
+
+  // auth change AppUser stream
+  Stream<AppUser> get user {
+    return _auth.authStateChanges().map(_appUser);
+  }
+
+  // sign in using email and password
+  Future signInWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = userCredential.user;
+      return _appUser(user);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // register with email and password
+  Future registerWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      User user = userCredential.user;
+
+      // Adding data to database
+
+      return _appUser(user);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
