0e254501c79a4414e981429e90c3f0dbedd82288
21varun12 <21varun12@gmail.com>
2021-06-19 23:54:11 +0800

    Add sign in screen based on wireframe

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/welcome/welcome.dart';
 import 'screens/home/home.dart';
 import 'screens/scan/add_receipt.dart';
+import 'screens/login/sign_in.dart';
 
 void main() => runApp(StockUP());
 
@@ -9,11 +10,12 @@ class StockUP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: WelcomeScreen.id,
+      initialRoute: SignInScreen.id,
       routes: {
         HomeScreen.id: (context) => HomeScreen(),
         AddReceiptScreen.id: (context) => AddReceiptScreen(),
         WelcomeScreen.id: (context) => WelcomeScreen(),
+        SignInScreen.id: (context) => SignInScreen(),
       },
     );
   }
