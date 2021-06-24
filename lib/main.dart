e6a1e4ea0cb04f83cad895595bf786e435fb504b
alinaleehx <alinaleehx@gmail.com>
2021-06-24 19:40:20 +0800

    Correct main.dart and update item screens Correct main.dart to allow firebase to run properly and update main.dart and item screen to utilise provider package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stockup/business_logic/item/item_viewmodel.dart';
+import 'package:stockup/models/appUser.dart';
 import 'package:stockup/screens/items/items.dart';
 import 'package:stockup/screens/scan/add_files.dart';
 import 'package:stockup/screens/search/search.dart';
@@ -8,25 +12,38 @@ import 'package:stockup/screens/home/home.dart';
 import 'package:stockup/screens/scan/add_receipt.dart';
 import 'package:stockup/screens/login/sign_in.dart';
 import 'package:stockup/screens/login/sign_up.dart';
+import 'package:stockup/services/auth/auth_impl.dart';
 
-void main() => runApp(StockUP());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(StockUP());
+}
 
 class StockUP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: ItemsScreen.id,
-      routes: {
-        WelcomeScreen.id: (context) => WelcomeScreen(),
-        SignInScreen.id: (context) => SignInScreen(),
-        SignUpScreen.id: (context) => SignUpScreen(),
-        HomeScreen.id: (context) => HomeScreen(),
-        AddReceiptScreen.id: (context) => AddReceiptScreen(),
-        AddFilesScreen.id: (context) => AddFilesScreen(),
-        ItemsScreen.id: (context) => ItemsScreen(),
-        ShoppingListScreen.id: (context) => ShoppingListScreen(),
-        SearchScreen.id: (context) => SearchScreen(),
-      },
+    return MultiProvider(
+      providers: [
+        StreamProvider<AppUser>.value(
+            value: AuthImplementation().user, initialData: null),
+        ChangeNotifierProvider<ItemViewModel>(
+            create: (context) => ItemViewModel()),
+      ],
+      child: MaterialApp(
+        initialRoute: ItemsScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          SignInScreen.id: (context) => SignInScreen(),
+          SignUpScreen.id: (context) => SignUpScreen(),
+          HomeScreen.id: (context) => HomeScreen(),
+          AddReceiptScreen.id: (context) => AddReceiptScreen(),
+          AddFilesScreen.id: (context) => AddFilesScreen(),
+          ItemsScreen.id: (context) => ItemsScreen(),
+          ShoppingListScreen.id: (context) => ShoppingListScreen(),
+          SearchScreen.id: (context) => SearchScreen(),
+        },
+      ),
     );
   }
 }
--- lib/screens/items/items.dart
+++ lib/screens/items/items.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:stockup/business_logic/item/item_viewmodel.dart';
 import '../constants.dart';
 
 class ItemsScreen extends StatefulWidget {
@@ -16,113 +18,118 @@ class _ItemsScreenState extends State<ItemsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Center(
-          child: Text(title),
-        ),
-      ),
-      body: Container(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Container(
-                          child: Icon(Icons.sort_rounded),
-                          padding: EdgeInsets.all(10),
+    return Consumer<ItemViewModel>(builder: (context, model, child) {
+      return WillPopScope(
+        onWillPop: null,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Center(
+              child: Text(title),
+            ),
+          ),
+          body: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Icon(Icons.sort_rounded),
+                              padding: EdgeInsets.all(10),
+                            ),
+                            Container(
+                              padding: EdgeInsets.all(10),
+                              child: DropdownButton<String>(
+                                value: dropdownValue,
+                                elevation: 16,
+                                onChanged: (String newValue) {
+                                  setState(() {
+                                    dropdownValue = newValue;
+                                  });
+                                },
+                                items: <String>[
+                                  'My List',
+                                  'One',
+                                  'Two',
+                                  'Free',
+                                  'Four'
+                                ].map<DropdownMenuItem<String>>((String value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: Text(value),
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                          ],
                         ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.share),
+                            Icon(Icons.sort_by_alpha),
+                            Icon(Icons.search),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      for (int i = 0; i < noCategories; ++i)
                         Container(
-                          padding: EdgeInsets.all(10),
-                          child: DropdownButton<String>(
-                            value: dropdownValue,
-                            elevation: 16,
-                            onChanged: (String newValue) {
-                              setState(() {
-                                dropdownValue = newValue;
-                              });
-                            },
-                            items: <String>[
-                              'My List',
-                              'One',
-                              'Two',
-                              'Free',
-                              'Four'
-                            ].map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
+                          margin: EdgeInsets.symmetric(horizontal: 5),
+                          child: FilterChip(
+                            label: Text('Category $i'),
+                            selected: false,
+                            onSelected: (bool selected) {},
                           ),
                         ),
-                      ],
-                    ),
+                    ],
                   ),
-                  Expanded(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(Icons.share),
-                        Icon(Icons.sort_by_alpha),
-                        Icon(Icons.search),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 50,
-              child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                children: [
-                  for (int i = 0; i < noCategories; ++i)
-                    Container(
-                      margin: EdgeInsets.symmetric(horizontal: 5),
-                      child: FilterChip(
-                        label: Text('Category $i'),
-                        selected: false,
-                        onSelected: (bool selected) {},
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: RefreshIndicator(
-                onRefresh: () {
-                  print('refresh triggered');
-                },
-                child: ListView(
-                  padding: EdgeInsets.only(top: 5.0),
-                  children: [
-                    for (int i = 1; i <= 3; ++i)
-                      ProductTile(
-                        'Product $i',
-                        'Expires in $i',
-                        Icon(Icons.more_vert),
-                      )
-                  ],
                 ),
-              ),
+                Expanded(
+                  child: RefreshIndicator(
+                    onRefresh: () {
+                      print('refresh triggered');
+                    },
+                    child: ListView(
+                      padding: EdgeInsets.only(top: 5.0),
+                      children: [
+                        for (int i = 1; i <= 3; ++i)
+                          ProductTile(
+                            'Product $i',
+                            'Expires in $i',
+                            Icon(Icons.more_vert),
+                          )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
+          bottomNavigationBar: kBottomNavigationBar,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
         ),
-      ),
-      bottomNavigationBar: kBottomNavigationBar,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    );
+      );
+    });
   }
 }
