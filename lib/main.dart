34538e888939e271e5c66765a8fd1534be63d19c
21varun12 <21varun12@gmail.com>
2021-06-28 16:11:15 +0800

    Update UI of user item screen

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class StockUP extends StatelessWidget {
             create: (context) => ItemViewModel()),
       ],
       child: MaterialApp(
-        initialRoute: WelcomeScreen.id,
+        initialRoute: ItemListScreen.id,
         routes: {
           AuthChange.id: (context) => AuthChange(),
           WelcomeScreen.id: (context) => WelcomeScreen(),
