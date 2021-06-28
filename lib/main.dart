a1d268b8e6361a1b95db42579b4ec334bc7fe3e4
21varun12 <21varun12@gmail.com>
2021-06-28 16:29:24 +0800

    Update add files UI

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class StockUP extends StatelessWidget {
             create: (context) => ItemViewModel()),
       ],
       child: MaterialApp(
-        initialRoute: WelcomeScreen.id,
+        initialRoute: AddFilesScreen.id,
         routes: {
           AuthChange.id: (context) => AuthChange(),
           WelcomeScreen.id: (context) => WelcomeScreen(),
