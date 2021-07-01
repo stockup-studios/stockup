7eac7114d5b483f09f76ae104f9f5f605bd26a3e
21varun12 <21varun12@gmail.com>
2021-07-01 18:43:43 +0800

    Refactor bottom navigation bar

--- lib/screens/scan/add_files.dart
+++ lib/screens/scan/add_files.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stockup/models/product.dart';
 import 'package:stockup/models/product_catalog/product_catalog.dart';
-import 'package:stockup/screens/home/home.dart';
-import 'package:stockup/screens/items/item_list.dart';
-import 'package:stockup/screens/shopping_list/shop_list.dart';
+import 'package:stockup/screens/components/bottom_navigation/bottom_navigation.dart';
 import 'package:stockup/services/parser/parser.dart';
 import 'package:stockup/services/scanner/scanner.dart';
 
@@ -17,26 +15,6 @@ class AddFilesScreen extends StatefulWidget {
 }
 
 class _AddFilesScreenState extends State<AddFilesScreen> {
-  void _onBottomNavigationBarItemTapped(int index) {
-    switch (index) {
-      case HomeScreen.index:
-        Navigator.of(context).pushReplacementNamed(HomeScreen.id);
-        break;
-      case ItemListScreen.index:
-        Navigator.of(context).pushReplacementNamed(ItemListScreen.id);
-        break;
-      case AddFilesScreen.index:
-        Navigator.of(context).pushReplacementNamed(AddFilesScreen.id);
-        break;
-      case ShopListScreen.index:
-        Navigator.of(context).pushReplacementNamed(ShopListScreen.id);
-        break;
-      default:
-        print('_onBottomNavigationBarItemTapped navigation error');
-        break;
-    }
-  }
-
   List<Product> productsFound = [];
 
   @override
@@ -140,32 +118,7 @@ class _AddFilesScreenState extends State<AddFilesScreen> {
         ),
       ),
       // TODO: Use BottomSheet to display search results
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.restaurant_menu),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_box_outlined),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.format_list_bulleted),
-            label: '',
-          ),
-        ],
-        backgroundColor: Colors.grey.shade300,
-        currentIndex: AddFilesScreen.index,
-        onTap: _onBottomNavigationBarItemTapped,
-      ),
+      bottomNavigationBar: BottomNavigation(currentIndex: AddFilesScreen.index),
     );
   }
 }
