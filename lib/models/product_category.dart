9e06dfcd7972f9f84a832cddca5ce8093e02e6a9
alinaleehx <alinaleehx@gmail.com>
2021-07-01 17:27:21 +0800

    Update user_item Update user_item object to use in firestore

--- lib/models/product_category.dart
+++ lib/models/product_category.dart
@@ -7,3 +7,44 @@ enum ProductCategory {
   beers_wines_spirits,
   food_pantry,
 }
+
+extension CategoryExtension on ProductCategory {
+  String get name {
+    switch (this) {
+      case ProductCategory.bakery_cereals_spreads:
+        return 'Bakery, Cereals & Spreads';
+      case ProductCategory.beers_wines_spirits:
+        return 'Beer, Wine & Spirit';
+      case ProductCategory.dairy_chilled_frozen:
+        return 'Dairy Chilled & Frozen';
+      case ProductCategory.food_pantry:
+        return 'Food Pantry';
+      case ProductCategory.fruit_vegetables:
+        return 'Fruit & Vegetables';
+      case ProductCategory.meats_seafood:
+        return 'Meats & Seafood';
+      case ProductCategory.snacks_drinks:
+        return 'Snacks & Drinks';
+      default:
+        return null;
+    }
+  }
+
+  static ProductCategory getCategory(String category) {
+    if (category.contains('Fruit & Vegetables')) {
+      return ProductCategory.fruit_vegetables;
+    } else if (category.contains('Food Pantry')) {
+      return ProductCategory.food_pantry;
+    } else if (category.contains('Diary, Chilled & Frozen')) {
+      return ProductCategory.dairy_chilled_frozen;
+    } else if (category.contains('Bakery, Cereals & Spreads')) {
+      return ProductCategory.bakery_cereals_spreads;
+    } else if (category.contains('Beer, Wine & Spirit')) {
+      return ProductCategory.beers_wines_spirits;
+    } else if (category.contains('Meats & Seafood')) {
+      return ProductCategory.meats_seafood;
+    } else {
+      return ProductCategory.snacks_drinks;
+    }
+  }
+}
