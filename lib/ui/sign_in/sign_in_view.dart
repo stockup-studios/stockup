c166b7e8f7f1f1c267cc1f01b207f9ac9284a8dc
21varun12 <21varun12@gmail.com>
2021-07-07 17:56:55 +0800

    Add sign in screen using stacked package

--- lib/ui/sign_in/sign_in_view.dart
+++ lib/ui/sign_in/sign_in_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stockup/ui/sign_in/sign_in_view_model.dart';
 
@@ -9,7 +10,151 @@ class SignInView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ViewModelBuilder<SignInViewModel>.reactive(
       builder: (context, model, child) => Scaffold(
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        backgroundColor: Colors.grey.shade200,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50.0, vertical: 5.0),
+                        child: Form(
+                          child: TextFormField(
+                            autocorrect: false,
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.email),
+                              hintText: 'Enter your email',
+                              labelText: 'Email',
+                            ),
+                            validator: model.emailValidator,
+                            onChanged: model.updateEmail,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 50.0, vertical: 5.0),
+                        child: Form(
+                          child: TextFormField(
+                            obscureText: true,
+                            autocorrect: false,
+                            decoration: const InputDecoration(
+                              icon: Icon(Icons.lock),
+                              hintText: 'Enter your password',
+                              labelText: 'Password',
+                            ),
+                            validator: model.passwordValidator,
+                            onChanged: model.updatePassword,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      FractionallySizedBox(
+                        widthFactor: 0.7,
+                        child: OutlinedButton(
+                          onPressed: () {}, // TODO: Implement auth in ViewModel
+                          child: Text('Sign In'),
+                          style: ButtonStyle(
+                            foregroundColor:
+                                MaterialStateProperty.all<Color>(Colors.white),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                Colors.grey.shade700),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        '', // TODO: Implement error handling in ViewModel
+                        style:
+                            TextStyle(color: Colors.red[400], fontSize: 16.0),
+                      ),
+                      TextButton(
+                        onPressed: () {}, // TODO: Implement in ViewModel
+                        child: Text(
+                          'Forgot password?',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.blue.shade900),
+                              ),
+                              onPressed: () {},
+                              child: Row(
+                                children: [
+                                  FaIcon(FontAwesomeIcons.facebookF),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text('Sign In'),
+                                ],
+                              ),
+                            ),
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.red.shade900),
+                              ),
+                              onPressed: () {}, // TODO: Implement in ViewModel
+                              child: Row(
+                                children: [
+                                  FaIcon(FontAwesomeIcons.google),
+                                  SizedBox(
+                                    width: 10,
+                                  ),
+                                  Text('Sign In'),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Don't have an account? ",
+                            ),
+                            TextButton(
+                              child: Text(
+                                "Create account",
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                              onPressed: model.signUp,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
       viewModelBuilder: () => SignInViewModel(),
     );
--- lib/ui/sign_in/sign_in_view_model.dart
+++ lib/ui/sign_in/sign_in_view_model.dart
@@ -1,3 +1,37 @@
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:stockup/app/app.locator.dart';
+import 'package:stockup/app/app.router.dart';
 
-class SignInViewModel extends BaseViewModel {}
+class SignInViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+  String _email = '';
+  String _password = '';
+
+  String emailValidator(val) {
+    return val.contains('@') ? null : 'Enter a valid email';
+  }
+
+  String passwordValidator(val) {
+    return val.length < 6 ? '6 or more characters password' : null;
+  }
+
+  set email(String val) {
+    _email = val;
+  }
+
+  void updateEmail(val) {
+    _email = val;
+    notifyListeners();
+  }
+
+  void updatePassword(val) {
+    _password = val;
+    notifyListeners();
+  }
+
+  void signUp() {
+    _navigationService.navigateTo(Routes.signInView);
+    notifyListeners();
+  }
+}
