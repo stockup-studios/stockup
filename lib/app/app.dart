0a715ad61466a40f21ba9beb93965a38d02f9d6e
21varun12 <21varun12@gmail.com>
2021-07-07 19:56:48 +0800

    Add home screen using stacked package
    Missing functionality in View that needs to be implemented with service classes in ViewModel have been marked with TODO

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:stockup/ui/sign_in/sign_in_view.dart';
 import 'package:stockup/ui/sign_up/sign_up_view.dart';
+import 'package:stockup/ui/user_home/user_home_view.dart';
 import 'package:stockup/ui/user_item/user_item_view.dart';
 import 'package:stockup/ui/welcome/welcome_view.dart';
 
@@ -10,7 +11,8 @@ import 'package:stockup/ui/welcome/welcome_view.dart';
     MaterialRoute(page: WelcomeView),
     MaterialRoute(page: SignInView),
     MaterialRoute(page: SignUpView),
-    MaterialRoute(page: UserItemView, initial: true),
+    MaterialRoute(page: UserHomeView, initial: true),
+    MaterialRoute(page: UserItemView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
---
+++ lib/ui/user_home/user_home_view.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stockup/screens/components/bottom_navigation/bottom_navigation.dart';
+import 'package:stockup/ui/user_home/user_home_view_model.dart';
+
+class UserHomeView extends StatelessWidget {
+  const UserHomeView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<UserHomeViewModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: model.messages.length,
+          itemBuilder: (context, index) {
+            return HomeTile(
+              // TODO: Replace with service
+              colorLeft: Colors.orangeAccent,
+              colorRight: Colors.deepOrangeAccent,
+              text: model.messages[index],
+            );
+          },
+        ),
+        bottomNavigationBar: BottomNavigation(
+            currentIndex: 0), // TODO: Replace with navigation service
+      ),
+      viewModelBuilder: () => UserHomeViewModel(),
+    );
+  }
+}
+
+class HomeTile extends StatelessWidget {
+  final colorLeft;
+  final colorRight;
+  final List<String> text;
+
+  const HomeTile({
+    Key key,
+    @required this.colorLeft,
+    @required this.colorRight,
+    @required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.only(top: 10, right: 15, bottom: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0),
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              colorLeft,
+              colorRight,
+            ],
+          ),
+        ),
+        child: ListTile(
+          title: Text(
+            text[0],
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+              fontSize: 40,
+              color: Colors.white,
+            ),
+          ),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              for (String detail in text.sublist(1))
+                Text(
+                  detail,
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
