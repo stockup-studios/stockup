6d3bceb002bf87678c21921e4ec67f90683ed0ef
21varun12 <21varun12@gmail.com>
2021-07-25 19:38:22 +0800

    Add items manually in shopping list screen

---
+++ lib/ui/user_shop/user_shop_add_view.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stockup/models/product_category.dart';
+import 'package:stockup/models/user_shop_list.dart';
+import 'package:stockup/ui/user_shop/user_shop_add_view_model.dart';
+
+class UserShopAddView extends StatelessWidget {
+  const UserShopAddView({@required this.userShopList, Key key})
+      : super(key: key);
+
+  final UserShopList userShopList;
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<UserShopAddViewModel>.reactive(
+      onModelReady: (model) => model.init(userShopList),
+      builder: (context, model, child) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 5),
+            child: Text(
+              'Add Item To Buy',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FractionallySizedBox(
+              widthFactor: 0.9,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                      hintText: 'Item name',
+                      border: InputBorder.none),
+                  onChanged: (String name) {
+                    model.name = name;
+                  },
+                ),
+              ),
+            ),
+          ),
+          if (model.nameError != '')
+            Text(
+              model.nameError,
+              style: TextStyle(color: Colors.red),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FractionallySizedBox(
+              widthFactor: 0.9,
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10)),
+                child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                      hintText: 'Item Image URL (leave blank for default)',
+                      border: InputBorder.none),
+                  onChanged: (String imageURL) {
+                    model.imageURL = imageURL;
+                  },
+                ),
+              ),
+            ),
+          ),
+          if (model.imageError != '')
+            Text(
+              model.imageError,
+              style: TextStyle(color: Colors.red),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FractionallySizedBox(
+              widthFactor: 0.9,
+              child: DropdownButton<ProductCategory>(
+                isExpanded: true,
+                value: model.category,
+                icon: Icon(Icons.arrow_downward),
+                onChanged: (ProductCategory newCategory) {
+                  model.category = newCategory;
+                },
+                items: ProductCategory.values
+                    .map<DropdownMenuItem<ProductCategory>>(
+                        (ProductCategory category) {
+                  return DropdownMenuItem<ProductCategory>(
+                    child: Text(category.name),
+                    value: category,
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: model.delQuantity,
+                      icon: Icon(Icons.exposure_minus_1),
+                    ),
+                    // Text(model.quantity.toString()),
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(10)),
+                        child: TextField(
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.phone,
+                          onChanged: (newVal) {
+                            model.changeQuantity(newVal);
+                          },
+                          decoration: InputDecoration.collapsed(
+                            hintText: model.quantity.toString(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: model.addQuantity,
+                      icon: Icon(Icons.exposure_plus_1),
+                    ),
+                  ],
+                )),
+          ),
+          if (model.quantityError != '')
+            Text(
+              model.quantityError,
+              style: TextStyle(color: Colors.red),
+            ),
+          FractionallySizedBox(
+            widthFactor: 0.7,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    child: Text('Add Item'),
+                    style: ButtonStyle(
+                      backgroundColor: model.name == ''
+                          ? MaterialStateProperty.all<Color>(Colors.grey)
+                          : MaterialStateProperty.all<Color>(Colors.green),
+                    ),
+                    onPressed: () async {
+                      if (await model.add()) {
+                        Navigator.pop(context);
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+          ),
+        ],
+      ),
+      viewModelBuilder: () => UserShopAddViewModel(),
+    );
+  }
+}
---
+++ lib/ui/user_shop/user_shop_add_view_model.dart
@@ -0,0 +1,173 @@
+import 'package:stacked/stacked.dart';
+import 'package:stockup/models/product_category.dart';
+import 'package:stockup/models/user_shop.dart';
+import 'package:stockup/models/user_shop_list.dart';
+import 'package:http/http.dart' as http;
+
+class UserShopAddViewModel extends BaseViewModel {
+  UserShopList userShopList;
+
+  /// required fields for adding new shop item
+  String _name;
+  ProductCategory _category;
+  int _quantity;
+  String _imageURL;
+
+  List<String> imageFormats = ['png', 'jpeg', 'jpg', 'tiff', 'tif'];
+
+  /// used for error messages on view
+  String _nameError = '';
+  String _imageError = '';
+  String _quantityError = '';
+
+  String get nameError {
+    return _nameError;
+  }
+
+  set nameError(String message) {
+    _nameError = message;
+    notifyListeners();
+  }
+
+  String get imageError {
+    return _imageError;
+  }
+
+  set imageError(String message) {
+    _imageError = message;
+    notifyListeners();
+  }
+
+  String get quantityError {
+    return _quantityError;
+  }
+
+  set quantityError(String message) {
+    _quantityError = message;
+    notifyListeners();
+  }
+
+  /// standard getters and setters with notifyListeners
+  String get name {
+    return _name;
+  }
+
+  set name(String name) {
+    _name = name;
+    checkName();
+    notifyListeners();
+  }
+
+  ProductCategory get category {
+    return _category;
+  }
+
+  set category(ProductCategory category) {
+    _category = category;
+    notifyListeners();
+  }
+
+  int get quantity {
+    return _quantity;
+  }
+
+  set quantity(int quantity) {
+    _quantity = quantity;
+    notifyListeners();
+  }
+
+  String get imageURL {
+    return _imageURL;
+  }
+
+  set imageURL(String imageURL) {
+    _imageURL = imageURL;
+    notifyListeners();
+  }
+
+  /// initialization code. Will be run on build
+  void init(UserShopList userShopList) {
+    this.userShopList = userShopList;
+    this.name = '';
+    this.imageURL = '';
+    this.category = ProductCategory.others;
+    this.quantity = 1;
+  }
+
+  void addQuantity() {
+    ++quantity;
+  }
+
+  void delQuantity() {
+    if (quantity > 1) {
+      --quantity;
+    }
+  }
+
+  void changeQuantity(String newVal) {
+    int newQuantity = -1;
+    try {
+      newQuantity = int.parse(newVal);
+      if (newQuantity > 0) {
+        quantity = newQuantity;
+        quantityError = '';
+      } else
+        throw ('Invalid quantity');
+    } catch (e) {
+      quantityError = 'Invalid quantity';
+    }
+  }
+
+  /// check that name is valid
+  void checkName() {
+    nameError = (name == '') ? 'Item name cannot be empty' : '';
+  }
+
+  /// check that image from url is valid and is a common image format
+  Future<void> checkImage() async {
+    if (imageURL == '') {
+      imageURL =
+          'https://toppng.com/uploads/preview/shopping-cart-115309972353g1kktalus.png';
+      imageError = '';
+      return;
+    }
+    var url;
+    var response;
+    try {
+      url = Uri.parse(imageURL);
+      response = await http.get(url);
+      if (response.statusCode != 200) {
+        imageError = 'Invalid image URL';
+        return;
+      }
+    } catch (e) {
+      imageError = 'Invalid image URL';
+      return;
+    }
+
+    if (!imageFormats.contains(imageURL.split('.').last)) {
+      imageError =
+          'Image URL must end in a common image format (jpg, jpeg, png)';
+      return;
+    }
+
+    imageError = '';
+  }
+
+  /// to add new item to userShopList
+  Future<bool> add() async {
+    checkName();
+    await checkImage();
+    if (nameError == '' && imageError == '' && quantityError == '') {
+      UserShop userShop = UserShop(
+          productName: name,
+          productID: -1,
+          category: category,
+          imageURL: imageURL);
+      userShop.quantity = quantity;
+      userShopList.addShopItem(userShop);
+      return true;
+    }
+    return false;
+  }
+}
